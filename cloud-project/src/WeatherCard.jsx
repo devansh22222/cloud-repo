b73409eb133e62e0e1e1fe392ffb1e9caee31951
Shiv-222004 <devanshoad123@gmail.com>
2025-04-28 21:53:26 +0530

    Initial commit - Full Stack Weather App

---
+++ cloud-project/src/WeatherCard.jsx
@@ -0,0 +1,48 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+export default function WeatherCard({ weatherData }) {
+    if (!weatherData) return null;
+
+    return (
+        <Card style={{ maxWidth: 600, marginTop: 20 , margin:"0 auto", height:"32rem", backgroundColor:"rgba(255, 255, 255, 1)"}}>
+            <CardContent>
+                <Typography variant="h5" component="div" style={{fontSize:"2rem"}}>
+                    Weather Information {weatherData.city}
+                </Typography>
+                <br />
+                <Typography sx={{ mb: 1.5 }} color="text.secondary"style={{fontSize:"1.5rem"}}>
+                    Timestamp: {weatherData.timestamp}
+                </Typography>
+                <br />
+                <Typography variant="body2" color="text.primary" style={{fontSize:"1.5rem"}}>
+                    Temperature: {weatherData.temp}°C
+                </Typography>
+                <br />
+                <Typography variant="body2" color="text.primary" style={{fontSize:"1.5rem"}}>
+                    Min Temperature: {weatherData.tempMin}°C
+                </Typography>
+                <br />
+                <Typography variant="body2" color="text.primary" style={{fontSize:"1.5rem"}}>
+                    Max Temperature: {weatherData.tempMax}°C
+                </Typography>
+                <br />
+                <Typography variant="body2" color="text.primary" style={{fontSize:"1.5rem"}}>
+                    Humidity: {weatherData.humidity}%
+                </Typography>
+                <br />
+                {weatherData.feelsLike !== "" && (
+                    <Typography variant="body2" color="text.primary" style={{fontSize:"1.5rem"}}>
+                        Feels Like: {weatherData.feelsLike}°C
+                    </Typography>
+                   
+                )}
+                
+                <Typography variant="body2" color="text.primary" style={{fontSize:"1.5rem"}}>
+                    Weather: {weatherData.weather}
+                </Typography><br />
+            </CardContent>
+        </Card>
+    );
+}
